3745fc0a26a2d17bd4bfb2a6b5269ed3074c1569
JohannesKing <105158760+JohannesKing@users.noreply.github.com>
2022-05-08 16:10:47 +0100

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://johannesking.github.io/microbit_james-bond/](https://johannesking.github.io/microbit_james-bond/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/johannesking/microbit_james-bond** and import
+
+## Edit this project ![Build status badge](https://github.com/johannesking/microbit_james-bond/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/johannesking/microbit_james-bond** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/johannesking/microbit_james-bond/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,164 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 2; index++) {
+        music.playTone(247, music.beat(BeatFraction.Double))
+        music.playTone(262, music.beat(BeatFraction.Double))
+        music.playTone(277, music.beat(BeatFraction.Double))
+        music.playTone(262, music.beat(BeatFraction.Double))
+    }
+    for (let index = 0; index < 2; index++) {
+        for (let index = 0; index < 1; index++) {
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Quarter))
+            music.playTone(370, music.beat(BeatFraction.Quarter))
+            music.playTone(370, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Whole))
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Half))
+        }
+        for (let index = 0; index < 1; index++) {
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(392, music.beat(BeatFraction.Quarter))
+            music.playTone(392, music.beat(BeatFraction.Quarter))
+            music.playTone(392, music.beat(BeatFraction.Half))
+            music.playTone(392, music.beat(BeatFraction.Whole))
+            music.playTone(370, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Half))
+        }
+    }
+    for (let index = 0; index < 1; index++) {
+        for (let index = 0; index < 1; index++) {
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Quarter))
+            music.playTone(370, music.beat(BeatFraction.Quarter))
+            music.playTone(370, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Whole))
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Half))
+        }
+        for (let index = 0; index < 1; index++) {
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(392, music.beat(BeatFraction.Quarter))
+            music.playTone(392, music.beat(BeatFraction.Quarter))
+            music.playTone(392, music.beat(BeatFraction.Half))
+            music.playTone(392, music.beat(BeatFraction.Whole))
+            music.playTone(370, music.beat(BeatFraction.Half))
+            music.playTone(349, music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(622, music.beat(BeatFraction.Half))
+            music.playTone(587, music.beat(BeatFraction.Double))
+            music.rest(music.beat(BeatFraction.Half))
+            music.playTone(494, music.beat(BeatFraction.Half))
+            music.playTone(440, music.beat(BeatFraction.Half))
+            music.playTone(494, music.beat(BeatFraction.Breve))
+        }
+    }
+    for (let index = 0; index < 2; index++) {
+        for (let index = 0; index < 1; index++) {
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Quarter))
+            music.playTone(370, music.beat(BeatFraction.Quarter))
+            music.playTone(370, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Whole))
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Half))
+        }
+        for (let index = 0; index < 1; index++) {
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(392, music.beat(BeatFraction.Quarter))
+            music.playTone(392, music.beat(BeatFraction.Quarter))
+            music.playTone(392, music.beat(BeatFraction.Half))
+            music.playTone(392, music.beat(BeatFraction.Whole))
+            music.playTone(370, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Half))
+        }
+    }
+    for (let index = 0; index < 1; index++) {
+        for (let index = 0; index < 1; index++) {
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Quarter))
+            music.playTone(370, music.beat(BeatFraction.Quarter))
+            music.playTone(370, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Whole))
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Half))
+        }
+        for (let index = 0; index < 1; index++) {
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(392, music.beat(BeatFraction.Quarter))
+            music.playTone(392, music.beat(BeatFraction.Quarter))
+            music.playTone(392, music.beat(BeatFraction.Half))
+            music.playTone(392, music.beat(BeatFraction.Whole))
+            music.playTone(370, music.beat(BeatFraction.Half))
+            music.playTone(349, music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Half))
+        }
+    }
+    for (let index = 0; index < 1; index++) {
+        music.playTone(622, music.beat(BeatFraction.Half))
+        music.playTone(587, music.beat(BeatFraction.Double))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(494, music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(494, music.beat(BeatFraction.Double))
+        music.playTone(523, music.beat(BeatFraction.Double))
+        music.playTone(554, music.beat(BeatFraction.Double))
+        music.playTone(523, music.beat(BeatFraction.Double))
+        music.playTone(494, music.beat(BeatFraction.Double))
+        music.playTone(523, music.beat(BeatFraction.Double))
+        music.playTone(554, music.beat(BeatFraction.Double))
+        music.playTone(523, music.beat(BeatFraction.Double))
+    }
+    for (let index = 0; index < 1; index++) {
+        music.playTone(330, music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(622, music.beat(BeatFraction.Half))
+        music.playTone(587, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(466, music.beat(BeatFraction.Half))
+        music.playTone(494, music.beat(BeatFraction.Double))
+        music.rest(music.beat(BeatFraction.Double))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(440, music.beat(BeatFraction.Quarter))
+        music.playTone(392, music.beat(BeatFraction.Quarter))
+        music.playTone(370, music.beat(BeatFraction.Whole))
+        music.playTone(247, music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Half))
+        music.playTone(277, music.beat(BeatFraction.Breve))
+        music.rest(music.beat(BeatFraction.Half))
+    }
+    for (let index = 0; index < 1; index++) {
+        music.playTone(330, music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(622, music.beat(BeatFraction.Half))
+        music.playTone(587, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(466, music.beat(BeatFraction.Half))
+        music.playTone(494, music.beat(BeatFraction.Double))
+        music.rest(music.beat(BeatFraction.Double))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(440, music.beat(BeatFraction.Quarter))
+        music.playTone(392, music.beat(BeatFraction.Quarter))
+        music.playTone(370, music.beat(BeatFraction.Whole))
+        music.playTone(247, music.beat(BeatFraction.Half))
+        music.playTone(311, music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Breve))
+        music.rest(music.beat(BeatFraction.Half))
+    }
+    for (let index = 0; index < 1; index++) {
+        music.playTone(330, music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+    }
+})
+basic.forever(function () {
+	
+})
